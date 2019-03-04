da15ca38f8a74fc7bb48f041f33fb469963cc01e
Jan Hellar <jhellar@redhat.com>
2019-03-04 13:56:06 +0100

    ➕ 📈ql 📨

--- server/index.js
+++ server/index.js
@@ -2,6 +2,9 @@ const express = require('express')
 //Include our server libraries
 const { VoyagerServer, gql } = require('@aerogear/voyager-server')
 const { conflictHandler, strategies } = require('@aerogear/voyager-conflicts')
+const { PubSub } = require('graphql-subscriptions');
+
+const pubSub = new PubSub();
 
 //Provide your graphql schema
 const typeDefs = gql`
@@ -10,12 +13,17 @@ const typeDefs = gql`
   }
 
   type Mutation {
-    updateTask(title: String!, version: Int!): Task
+    createTask(title: String!): Task
+    updateTask(id: Int!, title: String!, version: Int!): Task
   }
 
   type Task {
     title: String
     version: Int
+  }
+
+  type Subscription {
+    taskCreated: Task
   }
 `
 
@@ -38,30 +46,48 @@ const resolvers = {
     }
   },
   Mutation: {
+    createTask: async (obj, args, context, info) => {
+      args.version = 1;
+      tasks.push(args)
+      const result = args;
+      pubSub.publish('TaskCreated', {
+          taskCreated: result
+        });
+      return result
+    },
     updateTask: async (obj, clientData, context, info) => {
-      const task = { ...tasks[0] };
+      const args = {
+        title: clientData.title,
+        version: clientData.version
+      }
+      const task = { ...tasks[clientData.id] };
 
      // 2. Conflict Detection using `VersionedObjectState`
      //    If the version number from the database does not match the one sent by the client
      //    A conflict occurs
-     if (conflictHandler.hasConflict(task, clientData)) {
-       const { resolvedState, response } = await conflictHandler.resolveOnServer(customResolutionStrategy, task, clientData)
+     if (conflictHandler.hasConflict(task, args)) {
+       const { resolvedState, response } = await conflictHandler.resolveOnServer(customResolutionStrategy, task, args)
       
-       tasks[0] = resolvedState;
+       tasks[clientData.id] = resolvedState;
 
        return response
      }
 
      // 3. Always call nextState before persisting the updated record.
      // This ensures it has the correct version number
-     conflictHandler.nextState(clientData)
+     conflictHandler.nextState(args)
 
      // 4. Persist the update to the database and return it to the client
-     tasks[0] = clientData;
+     tasks[clientData.id] = args;
   
-      return tasks[0];
+      return tasks[clientData.id];
     }
-  }
+  },
+  Subscription: {
+    taskCreated: {
+        subscribe: () => pubSub.asyncIterator('TaskCreated')
+    }
+  },
 }
 
 //Initialize the library with your Graphql information
@@ -74,6 +100,24 @@ const server = VoyagerServer({
 const app = express()
 server.applyMiddleware({ app })
 
-app.listen(4000, () =>
-  console.log(`🚀 Server ready at http://localhost:4000/graphql`)
-)
+// app.listen(4000, () =>
+//   console.log(`🚀 Server ready at http://localhost:4000/graphql`)
+// )
+
+const http = require('http')
+const { SubscriptionServer } = require('subscriptions-transport-ws')
+const { execute, subscribe } = require('graphql');
+
+const httpServer = http.createServer(app)
+
+server.applyMiddleware({ app })
+  httpServer.listen(4000, () => {
+  new SubscriptionServer ({
+    execute,
+    subscribe,
+    schema: server.schema
+  }, {
+    server: httpServer,
+    path: '/graphql'
+  })
+})
